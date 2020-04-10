f544df39120d0d9f432e35c1178e79c4d4492eb7
keroro-o <obata19760205akinobu@outlook.jp>
2020-04-11 00:01:02 +0900

    認証が完了していないと見れないように実装

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', ensureAuthenticated, usersRouter);
 app.use('/photos', photosRouter);
 
 // パスに対する HTTP リクエストのハンドラの登録
@@ -86,6 +86,13 @@ app.get('/logout', function (req, res) {
   res.redirect('/');
 });
 
+/**
+ * 認証されていない場合、/login にリダイレクトさせる関数。
+ */
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
